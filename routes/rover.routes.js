096583b05db610c880d15b2b9f23650ea9d3c6a7
Tolstov Ivan <doc830@icloud.com>
2024-12-03 12:57:09 +0300

    Add rover api

--- routes/rover.routes.js
+++ routes/rover.routes.js
@@ -3,18 +3,22 @@ const stream = require('../middleware/stream/stream')
 const router = Router()
 router.post('/weather', (req, res) => {
     res.status(200)
+    console.log(req.body)
     stream.setData(req.body)
 })
 router.post('/visibility', (req, res) => {
     res.status(200)
+    console.log(req.body)
     stream.setData(req.body)
 })
 router.post('/ubx',  async (req, res)=> {
     res.status(200)
+    console.log(req.body)
    stream.setData(req.body)
 })
 router.post('/nmea', async (req, res)=> {
     res.status(200)
+    console.log(req.body)
     stream.setData(req.body)
 })
 
