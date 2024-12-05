c86775e05dfb4099d952fdc7d3206d353bb6ab2c
Tolstov Ivan <doc830@icloud.com>
2024-12-05 15:50:27 +0300

    Add logging

--- routes/rover.routes.js
+++ routes/rover.routes.js
@@ -1,5 +1,7 @@
 const {Router} = require('express')
 const stream = require('../middleware/stream/stream')
+const fs = require('fs')
+const path = require('path')
 const router = Router()
 router.post('/weather', (req, res) => {
     res.status(200)
@@ -11,11 +13,45 @@ router.post('/visibility', (req, res) => {
 })
 router.post('/ubx',  async (req, res)=> {
     res.status(200)
+    saveToFile('/logs/ubx.json', req.body)
+        .then(() => {})
+        .catch((err) => {
+            console.error('Error saving user data:', err);
+        })
    stream.setData(req.body)
 })
 router.post('/nmea', async (req, res)=> {
     res.status(200)
+    saveToFile('/logs/nmea.json', req.body)
+        .then(() => {})
+        .catch((err) => {
+            console.error('Error saving user data:', err);
+        })
     stream.setData(req.body)
 })
-
+function saveToFile(filename, data) {
+    const filePath = path.join(__dirname, filename)
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf-8', (err, fileContent) => {
+            if (err && err.code !== 'ENOENT') {
+                return reject(err)
+            }
+            let existingData = []
+            if (!err) {
+                try {
+                    existingData = JSON.parse(fileContent)
+                } catch (parseErr) {
+                    return reject(parseErr)
+                }
+            }
+            existingData.push(data)
+            fs.writeFile(filePath, JSON.stringify(existingData, null, 2), (writeErr) => {
+                if (writeErr) {
+                    return reject(writeErr)
+                }
+                resolve()
+            })
+        })
+    })
+}
 module.exports = router
