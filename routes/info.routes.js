14cfb8ada03df8f319cc939cb7708b94079e846b
Tolstov Ivan <doc830@icloud.com>
2024-12-16 11:55:23 +0300

    Add new path for logs

--- routes/info.routes.js
+++ routes/info.routes.js
@@ -1,17 +1,17 @@
 const {Router} = require('express')
 const router = Router()
-const devices = require('../middleware/devices/devices')
+const firmware = require('../middleware/devices/devices')
 router.get('/battery',  (req, res) => {
     let received = Buffer.alloc(0)
-     devices.openPort({
-            path: "/dev/ttyS1",
-            dataBits: 8,
-            baudRate: 115200,
-            stopBits: 1,
-            parity: "even"
-    },2).then(()=>{
-        devices.sendMessage(Buffer.from('A60100', 'hex'), devices.serialPort2).then(() => {
-            devices.serialPort2.on('data', (data)=>{
+    firmware.openPort({
+        path: "/dev/ttyS1",
+        dataBits: 8,
+        baudRate: 115200,
+        stopBits: 1,
+        parity: "even"
+    }).then((port)=>{
+        firmware.sendMessage(port, Buffer.from('A60100', 'hex')).then(() => {
+            port.on('data', (data)=>{
                 received = Buffer.concat([received, data])
                 if (received.length === 3) {
                     received = {
@@ -19,7 +19,7 @@ router.get('/battery',  (req, res) => {
                         "code": received.readUInt8(1),
                         "charge": ((received.readUInt8(2) >> 1)*0.042519+9).toFixed(2)
                     }
-                    devices.closePort(2).then(()=>{
+                    firmware.closePort(port).then(()=>{
                         res.json(received)
                     }).catch(err => {
                         res.json({
@@ -30,7 +30,6 @@ router.get('/battery',  (req, res) => {
                 }
             })
         }).catch(err => {
-            devices.serialPort2(2)
             res.json({
                 "err": "001",
                 "info": err.message
@@ -43,25 +42,25 @@ router.get('/battery',  (req, res) => {
         })
     })
 })
-router.get('/batch_number',  (req, res) => {
+router.get('/restart_navi',  (req, res) => {
     let received = Buffer.alloc(0)
-    devices.openPort({
+    firmware.openPort({
         path: "/dev/ttyS1",
         dataBits: 8,
         baudRate: 115200,
         stopBits: 1,
         parity: "even"
-    },2).then(()=>{
-        devices.sendMessage(Buffer.from('A60700', 'hex'), devices.serialPort2).then(() => {
-            devices.serialPort2.on('data', (data)=>{
+    }).then((port)=>{
+        firmware.sendMessage(port, Buffer.from('A6DAFF', 'hex')).then(() => {
+            port.on('data', (data)=>{
                 received = Buffer.concat([received, data])
                 if (received.length === 3) {
                     received = {
                         "header": received.readUInt8(0),
                         "code": received.readUInt8(1),
-                        "batch_number": received.readUInt8(2)
+                        "param": received.readUInt8(2)
                     }
-                    devices.closePort(2).then(()=>{
+                    firmware.closePort(port).then(()=>{
                         res.json(received)
                     }).catch(err => {
                         res.json({
@@ -72,49 +71,6 @@ router.get('/batch_number',  (req, res) => {
                 }
             })
         }).catch(err => {
-            devices.serialPort2(2)
-            res.json({
-                "err": "001",
-                "info": err.message
-            })
-        })
-    }).catch(err => {
-        res.json({
-            "err": "001",
-            "info": err.message
-        })
-    })
-})
-router.get('/serial_number',  (req, res) => {
-    let received = Buffer.alloc(0)
-    devices.openPort({
-        path: "/dev/ttyS1",
-        dataBits: 8,
-        baudRate: 115200,
-        stopBits: 1,
-        parity: "even"
-    },2).then(()=>{
-        devices.sendMessage(Buffer.from('A60800', 'hex'), devices.serialPort2).then(() => {
-            devices.serialPort2.on('data', (data)=>{
-                received = Buffer.concat([received, data])
-                if (received.length === 3) {
-                    received = {
-                        "header": received.readUInt8(0),
-                        "code": received.readUInt8(1),
-                        "serial_number": received.readUInt8(2)
-                    }
-                    devices.closePort(2).then(()=>{
-                        res.json(received)
-                    }).catch(err => {
-                        res.json({
-                            "err": "001",
-                            "info": err.message
-                        })
-                    })
-                }
-            })
-        }).catch(err => {
-            devices.serialPort2(2)
             res.json({
                 "err": "001",
                 "info": err.message
@@ -128,17 +84,16 @@ router.get('/serial_number',  (req, res) => {
     })
 })
 router.get('/device_off',  (req, res) => {
-    devices.openPort({
+    firmware.openPort({
         path: "/dev/ttyS1",
         dataBits: 8,
         baudRate: 115200,
         stopBits: 1,
         parity: "even"
-    },2).then(()=>{
-        devices.sendMessage(Buffer.from('A6AD00', 'hex'), devices.serialPort2).then(() => {
+    }).then((port)=>{
+        firmware.sendMessage( port, Buffer.from('A6AD00', 'hex')).then(() => {
             res.json("Device off")
         }).catch(err => {
-            devices.serialPort2(2)
             res.json({
                 "err": "001",
                 "info": err.message
